8e61a9ef67994920c898dae7cf6d9535907452bf
= <=>
2024-02-09 16:55:46 -0300

    refactor(protocols): leading to 404 when a protocol is not found

--- events/protocols/index.php
+++ events/protocols/index.php
@@ -9,16 +9,16 @@
 
     $protocol = $protocol->where('token', '=', $hash)->first();
 
-    if($protocol):
-        $client = new Client();
-        $event = new Event();
+    if(! $protocol) abort(404, 'Protocol not found!', 'error');
 
-        $client = $client->find($protocol->client_id);
-        $event = $event->find($protocol->event_id);
-        $location = $event->location();
-        $schedules = $event->schedules();
-        $total_schedules = count($schedules->data);
-    endif;
+    $client = new Client();
+    $event = new Event();
+
+    $client = $client->find($protocol->client_id);
+    $event = $event->find($protocol->event_id);
+    $location = $event->location();
+    $schedules = $event->schedules();
+    $total_schedules = count($schedules->data);
 
     loadHtml(__DIR__.'/body/read', [
         'image' => !is_null(SETTINGS) && !empty(SETTINGS['site_bg_login']) ? 'assets/images/'.SETTINGS['site_bg_login'] : 'assets/images/login_bg.jpg',
