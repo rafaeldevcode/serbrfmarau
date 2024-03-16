5efbd774e7c3b1676db0517dcbfeeb4ab214effc
= <=>
2024-03-16 08:01:42 -0300

    refactor(dashboard): changing image on the dashboard screen

--- admin/dashboard/body/read.php
+++ admin/dashboard/body/read.php
@@ -1,7 +1,7 @@
 <section class='p-3 bg-light m-0 sm:m-3 rounded shadow-lg'>
     <div class="flex justify-center items-center flex-wrap cm-browser-height border border-color-main rounded p-2">
         <div class="p-2 m-auto flex justify-center items-center w-full md:w-5/12">
-            <img class="w-full" src="<?php asset('assets/images/welcome.svg') ?>" alt="Wellcome to dashboard">
+            <img class="w-full" src="<?php !is_null(SETTINGS) && !empty(SETTINGS['site_logo_main']) ? asset('assets/images/'.SETTINGS['site_logo_main'].'') : asset('assets/images/logo_main.png') ?>" alt="Wellcome to dashboard">
         </div>
     </div>
 </section>
