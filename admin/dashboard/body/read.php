ddb9106e953912dbecdca5f1955b2b20df552508
= <=>
2024-03-20 07:20:53 -0300

    feat(dashbiard): added a small booking report to the dashboard

--- admin/dashboard/body/read.php
+++ admin/dashboard/body/read.php
@@ -1,7 +1,23 @@
 <section class='p-3 bg-light m-0 sm:m-3 rounded shadow-lg'>
-    <div class="flex justify-center items-center flex-wrap cm-browser-height border border-color-main rounded p-2">
-        <div class="p-2 m-auto flex justify-center items-center w-full md:w-5/12">
-            <img class="w-full" src="<?php !is_null(SETTINGS) && !empty(SETTINGS['site_logo_main']) ? asset('assets/images/'.SETTINGS['site_logo_main'].'') : asset('assets/images/logo_main.png') ?>" alt="Wellcome to dashboard">
+    <div class="p-2">
+        <h2 class="mb-2 text-3xl font-bold tracking-tight text-color-main border-b pb-2">Hoje</h2>
+
+        <div class="w-[360px] p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 pointer">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">Totais de reservas para hoje</h5>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-color-main text-end"><?php echo $today_reservations ?> <?php echo $today_reservations > 1 ? 'reservas' : 'reserva' ?></h5>
+        </div>
+    </div>
+
+    <div class="p-2">
+        <h2 class="mb-2 text-3xl font-bold tracking-tight text-color-main border-b pb-2">Locais</h2>
+
+        <div class="flex items-start flex-wrap gap-4">
+            <?php foreach($location_reservations as $reservation): ?>
+                <div class="w-[360px] p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 pointer">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900"><?php echo $reservation['location'] ?></h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-color-main text-end"><?php echo $reservation['count'] ?> <?php echo $reservation['count'] > 1 ? 'reservas' : 'reserva' ?></h5>
+                </div>
+            <?php endforeach ?>
         </div>
     </div>
 </section>
